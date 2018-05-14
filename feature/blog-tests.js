81435d1ae4d4ae9da05cc3956740b65b83f8334f
Lisa Reinhardt <reinal@gmail.com>
2018-05-13 19:01:44 -0500

    Installed chai and chai-http

---
+++ feature/blog-tests.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHTTP = require('chaiHTTP');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+const expect = chai.expect;
+
+const {BlogPost} = require('../models');
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
+
+chai.use(chaiHTTP);
