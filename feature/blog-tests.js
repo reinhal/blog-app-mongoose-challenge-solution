82596e5e5f1c0e1a531e3e6113c4c6e22837032b
Lisa Reinhardt <reinal@gmail.com>
2018-05-13 19:28:45 -0500

    added functions that will create fake blog posts with title, author, content

--- feature/blog-tests.js
+++ feature/blog-tests.js
@@ -12,3 +12,30 @@ const {app, runServer, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHTTP);
+
+function seedBlogData() {
+    console.info('Seeding Blog Data');
+    const seedData = [];
+
+    for (let i = 0; i <= 10; i++) {
+        seedData.push(generateBlogData());
+    }
+
+    return BlogPost.insertMany(seedData);
+}
+
+function generateBlogTitle() {
+    const titles = [
+        'Poor Old Tree', 'Trees Are Different', 'Happy Trees Here', 'One Hair Brush'];
+    return titles[Math.floor(Math.random() * titles.length)];
+}
+
+function generateBlogAuthor() {
+    const authors = [
+    'Bob Street', 'Carla Branden', 'John Fresh', 'Harriet Paths', 'Pat Idunno'];
+    return authors[Math.floor(Math.random() * authors.length)];
+}
+
+
+
+
